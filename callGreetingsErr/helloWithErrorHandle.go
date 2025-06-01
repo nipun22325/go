9ebfd5ec33a9b4143f4ab8a33e1a6702a9343904
Nipun <nipun22325@iiitd.ac.in>
2025-06-01 14:11:21 +0530

    returning and handling errors (corrected package name)

--- callGreetingsErr/helloWithErrorHandle.go
+++ callGreetingsErr/helloWithErrorHandle.go
@@ -10,11 +10,11 @@ func main(){
     // Set properties of the predefined Logger, including
     // the log entry prefix and a flag to disable printing
     // the time, source file, and line number.
-    log.SetPrefix("greetings: ")
+    log.SetPrefix("greetingsErr: ")
     log.SetFlags(0)
     
     // Request a greeting message.
-    message, err := greetings.Hello("")
+    message, err := greetingsErr.Hello("")
     if err != nil {
         log.Fatal(err) // if an error was returned, print it to the console and exit the program
     }
