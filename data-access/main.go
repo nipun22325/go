7c51b9ca42047debd8f21179c9dcb89cf12eb68e
Nipun <nipun22325@iiitd.ac.in>
2025-06-07 13:16:56 +0530

    add to database

--- data-access/main.go
+++ data-access/main.go
@@ -69,6 +69,13 @@ func main(){
         log.Fatal(err)
     }
     fmt.Printf("Album found: %v\n", alb)
+    
+    // Adding new album
+    albID, err := addAlbum(Album{Title: "The Modern Sound of Betty Carter", Artist: "Betty Carter", Price: 49.99,})
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Printf("ID of added album: %v\n", albID)
 }
 
 // Query the database for multiple rows
@@ -136,3 +143,19 @@ func albumByID(id int64) (Album, error) {
     }
     return alb, nil
 }
+
+// addAlbum adds the specified album to the database
+// returning the album ID of the new entry
+func addAlbum(alb Album) (int64, error) {
+    result, err := db.Exec("INSERT INTO album (title, artist, price) VALUES (?,?,?)", alb.Title, alb.Artist, alb.Price)
+    if err != nil {
+        return 0, fmt.Errorf("addAlbum: %v", err)
+    }
+    id, err := result.LastInsertId()
+    if err != nil {
+        return 0, fmt.Errorf("addAlbum: %v", err)
+    }
+    return id, nil
+}
+
+
